7c4f6b7eb16cc0208fac5a5b0f4c642c33a697bd
Mohamed Mohamed Afifi <68558691+Eng-mohamed-mohamed-afifi@users.noreply.github.com>
2021-12-10 19:47:16 +0200

    Add files via upload

--- maptest.py
+++ maptest.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # To Create map and display distance
 
+import sys
+
 import gmplot
 import tkinter as tk
 from tkinter import *
@@ -130,7 +132,14 @@ l3 = tk.Label(
     fg='white',
     )
 l3.place(x=320, y=100)
-om3 = tk.OptionMenu(my_w, options3, 'BFS', 'DFS', 'A*')
+om3 = tk.OptionMenu(
+    my_w,
+    options3,
+    'BFS',
+    'DFS',
+    'A*',
+    'D Algo',
+    )
 om3.place(x=340, y=135)
 
 b3 = tk.Button(
@@ -459,7 +468,7 @@ elif Algo == 'DFS' and startP != endP:
 
 
     final_list = DFS(adj_list, startP, endP)
-elif startP != endP:
+elif startP != endP and Algo == 'A*':
 
     # print(final_list)
 # A* Algorithm
@@ -642,12 +651,315 @@ elif startP != endP:
 
     final_list = []
     find_paths(startP, endP, cities, final_list, 0)
-else:
+elif Algo == 'D Algo' and startP != endP:
+
 
     # print(final_list)
     # ////////////
 
-    print 'Start Can not Equal The End'
+    def dijkstra(start, end):
+
+        # Graph Object
+
+        class Graph(object):
+
+            def __init__(self, nodes, init_graph):
+                self.nodes = nodes
+                self.graph = self.construct_graph(nodes, init_graph)
+
+            def construct_graph(self, nodes, init_graph):
+
+                # This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
+
+                graph = {}
+                for node in nodes:
+                    graph[node] = {}
+
+                graph.update(init_graph)
+
+                for (node, edges) in graph.items():
+                    for (adjacent_node, value) in edges.items():
+                        if graph[adjacent_node].get(node, False) \
+                            == False:
+                            graph[adjacent_node][node] = value
+
+                return graph
+
+            def get_nodes(self):
+
+                # "Returns the nodes of the graph."
+
+                return self.nodes
+
+            def get_outgoing_edges(self, node):
+
+                # "Returns the neighbors of a node."
+
+                connections = []
+                for out_node in self.nodes:
+                    if self.graph[node].get(out_node, False) != False:
+                        connections.append(out_node)
+                return connections
+
+            def value(self, node1, node2):
+
+                # "Returns the value of an edge between two nodes."
+
+                return self.graph[node1][node2]
+
+        # Algorithm function
+
+        def dijkstra_algorithm(graph, start_node):
+            unvisited_nodes = list(graph.get_nodes())
+
+            # We'll use this dict to save the cost of visiting each node and update it as we move along the graph
+
+            shortest_path = {}
+
+            # We'll use this dict to save the shortest known path to a node found so far
+
+            previous_nodes = {}
+
+            # We'll use max_value to initialize the "infinity" value of the unvisited nodes
+
+            max_value = sys.maxsize
+            for node in unvisited_nodes:
+                shortest_path[node] = max_value
+
+            # However, we initialize the starting node's value with 0
+
+            shortest_path[start_node] = 0
+
+            # The algorithm executes until we visit all nodes
+
+            while unvisited_nodes:
+
+                # The code block below finds the node with the lowest score
+
+                current_min_node = None
+                for node in unvisited_nodes:  # Iterate over the nodes
+                    if current_min_node == None:
+                        current_min_node = node
+                    elif shortest_path[node] \
+                        < shortest_path[current_min_node]:
+                        current_min_node = node
+
+                # The code block below retrieves the current node's neighbors and updates their distances
+
+                neighbors = graph.get_outgoing_edges(current_min_node)
+                for neighbor in neighbors:
+                    tentative_value = shortest_path[current_min_node] \
+                        + graph.value(current_min_node, neighbor)
+                    if tentative_value < shortest_path[neighbor]:
+                        shortest_path[neighbor] = tentative_value
+
+                        # We also update the best path to the current node
+
+                        previous_nodes[neighbor] = current_min_node
+
+                # After visiting its neighbors, we mark the node as "visited"
+
+                unvisited_nodes.remove(current_min_node)
+
+            return previous_nodes
+
+        # Print Function
+
+        def print_result(previous_nodes, start_node, target_node):
+            path = []
+            node = target_node
+
+            while node != start_node:
+                path.append(node)
+                node = previous_nodes[node]
+
+            # Add the start node manually
+
+            path.append(start_node)
+
+            # print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
+
+            return path[::-1]
+
+        # Main Code
+
+        adj_list = {
+            'shebin': {
+                'tala': 18,
+                'el shohada': 16.5,
+                'minuf': 16.4,
+                'el-bagour': 14.6,
+                'quwaysna': 18.7,
+                'birket as sab': 14,
+                },
+            'minuf': {
+                'el shohada': 18.2,
+                'shebin': 16.1,
+                'el-bagour': 11.4,
+                'ashmun': 27,
+                'el sadat city': 55.9,
+                },
+            'tala': {
+                'birket as sab': 18.1,
+                'shebin': 18,
+                'el shohada': 14.4,
+                'Kafr El-Zayat': 25.6,
+                'tanta': 15.3,
+                'As Santah': 29.2,
+                },
+            'birket as sab': {
+                'tala': 18.1,
+                'shebin': 14,
+                'quwaysna': 13.3,
+                'As Santah': 16,
+                'zefta': 24.5,
+                },
+            'el-bagour': {
+                'quwaysna': 29.8,
+                'shebin': 14.9,
+                'minuf': 11.5,
+                'ashmun': 22.5,
+                'banha': 26.6,
+                'toukh': 39.7,
+                'Al Qanatir Al Khayriyyah': 30.8,
+                },
+            'ashmun': {
+                'el-bagour': 23.8,
+                'minuf': 27.1,
+                'el sadat city': 61.7,
+                'Al Qanatir Al Khayriyyah': 23.5,
+                'toukh': 56.3,
+                },
+            'quwaysna': {
+                'birket as sab': 13.9,
+                'shebin': 18.1,
+                'el-bagour': 18.1,
+                'zefta': 21.1,
+                'kafr shokr': 26.8,
+                'banha': 15.1,
+                },
+            'el sadat city': {'ashmun': 73.8, 'minuf': 54.5},
+            'el shohada': {'tala': 14.4, 'shebin': 17.5,
+                           'minuf': 18.2},
+            'Kafr El-Zayat': {'basioun': 20.2, 'tanta': 27.6,
+                              'tala': 25.6},
+            'basioun': {'qutur': 16.8, 'tanta': 28.8,
+                        'Kafr El-Zayat': 20.2},
+            'qutur': {'basioun': 17.1, 'tanta': 16.3,
+                      'El-Mahalla El-Kubra': 28.8},
+            'El-Mahalla El-Kubra': {
+                'qutur': 30.8,
+                'tanta': 31.5,
+                'As Santah': 30.8,
+                'zefta': 33.3,
+                'Samannoud': 7.7,
+                },
+            'Samannoud': {'El-Mahalla El-Kubra': 7.8,
+                          'As Santah': 46.6, 'zefta': 33.1},
+            'zefta': {
+                'Samannoud': 35,
+                'El-Mahalla El-Kubra': 32.7,
+                'As Santah': 17.9,
+                'quwaysna': 21.1,
+                'birket as sab': 25.3,
+                'kafr shokr': 27.9,
+                },
+            'As Santah': {
+                'Samannoud': 48.7,
+                'El-Mahalla El-Kubra': 30.3,
+                'birket as sab': 16.2,
+                'tanta': 25.3,
+                },
+            'tanta': {
+                'tala': 15,
+                'Kafr El-Zayat': 26.4,
+                'basioun': 28.3,
+                'qutur': 16,
+                'El-Mahalla El-Kubra': 29.2,
+                'As Santah': 25.9,
+                },
+            'kafr shokr': {'zefta': 32.7, 'quwaysna': 23.1,
+                           'banha': 13.4},
+            'banha': {
+                'kafr shokr': 17.7,
+                'quwaysna': 17.8,
+                'el-bagour': 29.4,
+                'toukh': 15.6,
+                'shibin el qanatir': 28.8,
+                },
+            'toukh': {
+                'banha': 17.5,
+                'el-bagour': 40,
+                'ashmun': 57.5,
+                'Al Qanatir Al Khayriyyah': 25,
+                'qalyub': 22,
+                'shibin el qanatir': 14,
+                },
+            'Al Qanatir Al Khayriyyah': {
+                'toukh': 24.7,
+                'ashmun': 23.5,
+                'Shubra Al Khaymah': 18.5,
+                'qalyub': 7.6,
+                },
+            'Shubra Al Khaymah': {'Al Qanatir Al Khayriyyah': 18.4,
+                                  'qalyub': 11.6, 'el khankah': 22.4},
+            'qalyub': {
+                'el khankah': 26.9,
+                'shibin el qanatir': 20.6,
+                'toukh': 22.7,
+                'Al Qanatir Al Khayriyyah': 7.6,
+                'Shubra Al Khaymah': 12.1,
+                },
+            'shibin el qanatir': {
+                'banha': 29.8,
+                'toukh': 14,
+                'qalyub': 20.4,
+                'el khankah': 17.6,
+                },
+            'el khankah': {'shibin el qanatir': 15.9, 'qalyub': 27.4,
+                           'Shubra Al Khaymah': 24.4},
+            }
+
+        ourCities = [
+            'shebin',
+            'minuf',
+            'tala',
+            'birket as sab',
+            'el-bagour',
+            'ashmun',
+            'quwaysna',
+            'el sadat city',
+            'el shohada',
+            'Kafr El-Zayat',
+            'basioun',
+            'tanta',
+            'qutur',
+            'El-Mahalla El-Kubra',
+            'As Santah',
+            'Samannoud',
+            'zefta',
+            'banha',
+            'qalyub',
+            'Al Qanatir Al Khayriyyah',
+            'Shubra Al Khaymah',
+            'el khankah',
+            'kafr shokr',
+            'shibin el qanatir',
+            'toukh',
+            ]
+        graph = Graph(ourCities, adj_list)
+        previous_nodes = dijkstra_algorithm(graph, start)
+        path = print_result(previous_nodes, startP, endP)
+        return path
+
+
+    final_list = dijkstra(startP, endP)
+else:
+
+    print ('The start can not equal the end !')
+
+###############################################
+#
 
 ###############################################
 # Real coordanitas
